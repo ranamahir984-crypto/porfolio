42f98a2ffdbaf2a2065a23b0b83c4cddb3102c08
ranamahir984-crypto <ranamahir984@gmail.com>
2026-02-21 19:45:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,4 +35,35 @@ sec.classList.add("show");
 });
 
 });
+window.onscroll=function(){
+
+let winScroll=document.documentElement.scrollTop;
+
+let height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+let scrolled=(winScroll/height)*100;
+
+document.getElementById("progress").style.width=scrolled+"%";
+
+};
+let text="Frontend Developer";
+
+let i=0;
+
+function type(){
+
+if(i<text.length){
+
+document.getElementById("typing").innerHTML+=text.charAt(i);
+
+i++;
+
+setTimeout(type,100);
+
+}
+
+}
+
+type();
 </script>
+
