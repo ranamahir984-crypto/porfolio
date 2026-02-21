47c360cc76aeec008d4ba9e71803ddbdbc454637
ranamahir984-crypto <ranamahir984@gmail.com>
2026-02-21 19:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,23 @@ cards.forEach(card => {
   card.style.transform = "translateY(50px)";
   card.style.transition = "0.6s ease";
 });
+window.addEventListener("scroll",function(){
+
+let sections=document.querySelectorAll(".section");
+
+sections.forEach(function(sec){
+
+let position=sec.getBoundingClientRect().top;
+
+let screen=window.innerHeight;
+
+if(position<screen-100){
+
+sec.classList.add("show");
+
+}
+
+});
+
+});
 </script>
